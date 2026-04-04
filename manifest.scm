1724fa3128090e7c4a718ff2ccf2be88dcb1da15
GigiaJ <gigiaboone@yahoo.com>
2026-04-04 05:11:52 -0500

    add guix manifest

---
+++ manifest.scm
@@ -0,0 +1,18 @@
+(specifications->manifest
+ '(
+   "curl"
+   "findutils"
+   "grep"
+   "gcc-toolchain"
+   "pkg-config"
+   "which"
+   "zlib"
+   "binaryen" ; This provides wasm-opt
+   "node"
+   "openssl"
+   "yarn"
+   "git"
+   "bash"
+   "sed"
+   "nss-certs"
+   "coreutils"))
